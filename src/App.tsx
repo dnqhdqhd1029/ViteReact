2438d857bf4e769edc0881eb016544277bb9d0ce
gojs-hwik <go.js@hwik.com>
2023-03-24 13:09:22 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import '@/styles.scss';
 import '@/utils/i18n';
 import { useAxiosInterceptor } from '@/API';
+
+import Home from '@/pages/Home';
 import TestApp from '@/pages/test/TestApp';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/test',
     element: <TestApp />,
   },
 ]);
